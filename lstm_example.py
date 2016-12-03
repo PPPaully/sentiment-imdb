8145cc920eb34cf0e9a3ec9f0b544d4bcc421f7f
yoyoismee <yoyoismee@gmail.com>
2016-12-04 05:52:45 +0700

    convert to numpy

--- lstm_example.py
+++ lstm_example.py
@@ -1,36 +1,43 @@
-n_word = 50 <- See lstm example pic
+# coding=utf-8
+n_word = 50   #<- See lstm example pic
 n_vec = 300
 n_unit = 32
 n_hidden = 64
 n_class = 30
+import numpy as np
+#INPUT = np.ndarray(n_word, n_vec) #T.fmatrix() <- 2D-array = numpy.ndarray shape likes (n_word, n_vec)
+#CELL = np.ndarray(n_unit, n_vec) #T.fmatrix() #<- shape likes (n_unit, n_vec)
+#HIDDEN = np.ndarray(n_unit, n_vec) #T.fmatrix() #<- shape likes (n_unit, n_vec)
+lstm_wxi = np.random.rand(n_unit,n_vec,n_vec)   #init_weights((n_unit, n_vec, n_vec)) <- 3D-array with (n_unit,n_vec,n_vec). Those will come from pretrained-weight
+lstm_wxf = np.random.rand(n_unit,n_vec,n_vec)   #init_weights((n_unit, n_vec, n_vec))
+lstm_wxc = np.random.rand(n_unit,n_vec,n_vec)   #init_weights((n_unit, n_vec, n_vec))
+lstm_wxo = np.random.rand(n_unit,n_vec,n_vec)   #init_weights((n_unit, n_vec, n_vec))
 
-INPUT = T.fmatrix() <- 2D-array = numpy.ndarray shape likes (n_word, n_vec)
-CELL = T.fmatrix() <- shape likes (n_unit, n_vec)
-HIDDEN = T.fmatrix() <- shape likes (n_unit, n_vec)
+lstm_whi = np.random.rand(n_unit,n_vec,n_vec)#init_weights((n_unit, n_vec, n_vec))
+lstm_whf = np.random.rand(n_unit,n_vec,n_vec)#init_weights((n_unit, n_vec, n_vec))
+lstm_whc = np.random.rand(n_unit,n_vec,n_vec) #init_weights((n_unit, n_vec, n_vec))
+lstm_who = np.random.rand(n_unit,n_vec,n_vec)#init_weights((n_unit, n_vec, n_vec))
 
+lstm_wci = np.random.rand(n_unit,n_vec,n_vec)#init_weights((n_unit, n_vec, n_vec))
+lstm_wcf = np.random.rand(n_unit,n_vec,n_vec) #init_weights((n_unit, n_vec, n_vec))
+lstm_wco = np.random.rand(n_unit,n_vec,n_vec)  #init_weights((n_unit, n_vec, n_vec))
 
-lstm_wxi = init_weights((n_unit, n_vec, n_vec)) <- 3D-array with (n_unit,n_vec,n_vec). Those will come from pretrained-weight
-lstm_wxf = init_weights((n_unit, n_vec, n_vec))
-lstm_wxc = init_weights((n_unit, n_vec, n_vec))
-lstm_wxo = init_weights((n_unit, n_vec, n_vec))
+def sigmoid(x):
+    return 1/(1+pow(2.7182818284590452353602874713527,-x))
 
-lstm_whi = init_weights((n_unit, n_vec, n_vec))
-lstm_whf = init_weights((n_unit, n_vec, n_vec))
-lstm_whc = init_weights((n_unit, n_vec, n_vec))
-lstm_who = init_weights((n_unit, n_vec, n_vec))
-
-lstm_wci = init_weights((n_unit, n_vec, n_vec))
-lstm_wcf = init_weights((n_unit, n_vec, n_vec))
-lstm_wco = init_weights((n_unit, n_vec, n_vec))
-
+def batched_dot(a,b):
+    out = np.zero(len(a),len(a[1]),len(b[1][1]))
+    for i in range(len(a)):
+        out[i] = np.dot(a[i],b[i])
+    return out
 
 def lstm(x_t, c_tm1, h_tm1):
-    x_t_repeat = T.repeat(x_t, n_unit, axis=0)
-    i_t = T.nnet.sigmoid(T.batched_dot(x_t_repeat, lstm_wxi) + T.batched_dot(h_tm1, lstm_whi) + T.batched_dot(c_tm1, lstm_wci))
-    f_t = T.nnet.sigmoid(T.batched_dot(x_t_repeat, lstm_wxf) + T.batched_dot(h_tm1, lstm_whf) + T.batched_dot(c_tm1, lstm_wcf))
-    c_t = T.mul(f_t, c_tm1) + i_t*T.tanh(T.batched_dot(x_t_repeat, lstm_wxc) + T.batched_dot(h_tm1, lstm_whc))
-    o_t = T.nnet.sigmoid(T.batched_dot(x_t_repeat, lstm_wxo) + T.batched_dot(h_tm1, lstm_who) + T.batched_dot(c_t, lstm_wco))
-    h_t = o_t*T.tanh(c_t)
+    x_t_repeat = np.repeat(x_t, n_unit, axis=0)
+    i_t = sigmoid(batched_dot(x_t_repeat, lstm_wxi) + batched_dot(h_tm1, lstm_whi) + batched_dot(c_tm1, lstm_wci))
+    f_t = sigmoid(batched_dot(x_t_repeat, lstm_wxf) + batched_dot(h_tm1, lstm_whf) + batched_dot(c_tm1, lstm_wcf))
+    c_t = np.dot(f_t, c_tm1) + i_t*np.tanh(batched_dot(x_t_repeat, lstm_wxc) + batched_dot(h_tm1, lstm_whc))
+    o_t = sigmoid(batched_dot(x_t_repeat, lstm_wxo) + batched_dot(h_tm1, lstm_who) + batched_dot(c_t, lstm_wco))
+    h_t = o_t*np.tanh(c_t)
     return c_t, h_t
 
 # funtion:
@@ -44,7 +51,7 @@ def lstm(x_t, c_tm1, h_tm1):
 #   T.mul - Hadamard product A*B at ij = Aij*Bij
 #   T.tanh - Hyper-tangent function
 
-lstm_layer = ...
+#####lstm_layer = ...
 # for loop in INPUT:
 #   do lstm(input_one_word, CELL, HIDDEN)
 #   with input_one_word = INPUT[i] shape likes (1, 1-word, n_vec) - see numpy.reshape
@@ -52,10 +59,10 @@ lstm_layer = ...
 # OUTPUT - lstm_layer = each HIDDEN state [HIDDEN at 1, HIDDEN at 2, HIDDEN at 3, ..., HIDDEN at n_word]
 #    so output's shape must be (n_word, n_unit, n_vec)
 
-lstm_output = T.flatten(lstm_layer, 2) <- 2 mean 2-dims
+#####lstm_output = T.flatten(lstm_layer, 2) <- 2 mean 2-dims
 # see numpy.flatten
 # make sure that lstm_output's shape must be (n_word, n_unit * n_vec)
 
-model_fw = init_weights((n_unit * n_vec, n_class))
-model_output = T.nnet.sigmoid(T.dot(lstm_output, model_fw))
+#model_fw = init_weights((n_unit * n_vec, n_class))
+#model_output = T.nnet.sigmoid(T.dot(lstm_output, model_fw))
 # Just dot = fully connected NN
