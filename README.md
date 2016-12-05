bbce5cb5e6e3ad0b000f92f07e924da89e532483
Naphan Benchasattabuse <naphanben@gmail.com>
2016-12-05 17:08:19 +0700

    add flask template to project please read the readme

--- README.md
+++ README.md
@@ -1,1 +1,30 @@
 # Sentiment Analysis with IMDB dataset
+
+## to set up the project
+- it is reccommend that you use virtualenv so that your global python will not be polluted
+
+example for Windows:
+```
+    pip install virtualenv
+    cd project
+    virtualenv venv
+    venv/scripts/activate
+```
+- to exit the virtual environment
+
+```
+    deactivate
+```
+
+
+- install all the dependency of flask using
+
+```
+    pip install flask
+```
+
+- run app (it will run at localhost:5000)
+```
+    python app.py
+```
+
---
+++ app/__init__.py
@@ -0,0 +1,47 @@
+from flask import Flask, request, g, render_template
+
+DEBUG = True
+
+app = Flask(__name__, static_folder='../static', template_folder='../templates')
+app.config.from_object(__name__)
+# app.config.from_pyfile("config.py")
+app.secret_key = "teacup-testing"
+
+
+# clear cache
+@app.after_request
+def add_header(response):
+    """
+    Add headers to both force latest IE rendering engine or Chrome Frame,
+    and also to cache the rendered page for 10 minutes.
+
+    :param response: flask response object
+    """
+    response.headers['X-UA-Compatible'] = 'IE=Edge,chrome=1'
+    response.headers['Cache-Control'] = 'public, max-age=0'
+    return response
+
+
+@app.route('/')
+def main():
+    return render_template('main.html')
+
+
+@app.errorhandler(401)
+def internal_server_error(e):
+    return render_template('errors/401.html'), 401
+
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('errors/404.html'), 404
+
+
+@app.errorhandler(403)
+def forbidden(e):
+    return render_template('errors/403.html'), 403
+
+
+@app.errorhandler(500)
+def internal_server_error(e):
+    return render_template('errors/500.html'), 500
