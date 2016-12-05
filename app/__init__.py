c515bfcc68c08ec4415c314221fcb9ce1c4948e3
Naphan Benchasattabuse <naphanben@gmail.com>
2016-12-05 19:27:16 +0700

    remove error routes and add template function for analysis

--- app/__init__.py
+++ app/__init__.py
@@ -22,26 +22,38 @@ def add_header(response):
     return response
 
 
+@app.route('/helloworld', methods=['POST'])
+def hello():
+    data = request.form 
+    data = dict(data)
+    review = data['foo'][0]
+    result = this_is_magic(review)
+    return result
+
+
 @app.route('/')
 def main():
     return render_template('main.html')
 
+def this_is_magic(review):
+    # TODO implement your function here
+    return 'result: {}'.format(review)
 
-@app.errorhandler(401)
-def internal_server_error(e):
-    return render_template('errors/401.html'), 401
-
-
-@app.errorhandler(404)
-def page_not_found(e):
-    return render_template('errors/404.html'), 404
+# @app.errorhandler(401)
+# def internal_server_error(e):
+#     return render_template('errors/401.html'), 401
 
 
-@app.errorhandler(403)
-def forbidden(e):
-    return render_template('errors/403.html'), 403
+# @app.errorhandler(404)
+# def page_not_found(e):
+#     return render_template('errors/404.html'), 404
 
 
-@app.errorhandler(500)
-def internal_server_error(e):
-    return render_template('errors/500.html'), 500
+# @app.errorhandler(403)
+# def forbidden(e):
+#     return render_template('errors/403.html'), 403
+
+
+# @app.errorhandler(500)
+# def internal_server_error(e):
+#     return render_template('errors/500.html'), 500
